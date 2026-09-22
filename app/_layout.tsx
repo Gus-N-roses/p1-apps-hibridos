5f40404625fcbf573b45a4468a935e4d45f88460
Gustavo França <gutimendes@icloud.com>
2026-09-22 10:19:44 -0300

    remove fonte e lógica de splash screen não utilizadas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,4 @@
-import { useFonts } from 'expo-font';
 import { DefaultTheme, Stack, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { colors, typography } from '@/constants/theme';
@@ -15,29 +12,7 @@ export const unstable_settings = {
   initialRouteName: '(tabs)',
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <ThemeProvider value={DefaultTheme}>
       <Stack
